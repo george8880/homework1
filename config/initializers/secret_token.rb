602ef0428831b17a7761ba46b6531b9f01b2ed3d
subl <pwan@seas.upenn.edu>
2013-01-17 16:48:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HW01::Application.config.secret_token = 'f560d91505c51a50828b5c40c096f1d349ddfe50e2383676abd855ec72812f55ff2aa05a91d1a7e6b881fb4bf32ba7cce0d846811a510452a8eb4dbfe525cdd3'
